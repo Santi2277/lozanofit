b2580a9fb23264bac1224540d297a9135af25c9c
Santi Lozano <santi_3_92@hotmail.com>
2019-01-26 02:49:43 +0100

    before new way to obtaining exerc list

---
+++ app/src/main/java/deploy/android/lozanofit/es/lozanofitroutinemixer/activities/Activity5.java
@@ -0,0 +1,22 @@
+package deploy.android.lozanofit.es.lozanofitroutinemixer.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import deploy.android.lozanofit.es.lozanofitroutinemixer.R;
+
+public class Activity5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_5);
+    }
+
+
+
+
+
+
+
+}
